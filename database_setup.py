61131e847168d29cacf96e48069f455a0885ecc2
Terence Fox <terence.fo@gmail.com>
2018-01-12 16:52:19 -0500

    Add users to database and update server code

--- database_setup.py
+++ database_setup.py
@@ -7,17 +7,29 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
        return {
            'name'         : self.name,
        }
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
@@ -30,8 +42,9 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
 
-#We added this serialize function to be able to send JSON objects in a serializable format
     @property
     def serialize(self):
        return {
@@ -43,7 +56,7 @@ class MenuItem(Base):
        }
 
 
-engine = create_engine('sqlite:///restaurant.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 
 
 Base.metadata.create_all(engine)
